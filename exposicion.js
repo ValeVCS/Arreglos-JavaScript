6dc27687003fddf2aa0e212b55d1b4b751949631
ValeVCS <valeria.de.la.luz.soria.34.181@gmail.com>
2025-03-18 09:09:29 -0600

    Add files via upload

---
+++ exposicion.js
@@ -0,0 +1,53 @@
+
+//Formas de crear un arreglo
+/*
+let numeros = [10, 20, 30, 40, 50]
+console.log(numeros)
+*/
+/*let planetas = ["Marte", "Jupiter", "Saturno"]
+console.log(planetas)
+
+//Acceder a los elementos de un arreglo
+console.log("Primer planeta:", planetas[0]);
+console.log("Último planeta:", planetas[planetas.length - 1])
+*/
+
+//Métodos comunes de los arreglos
+/*let planetas = ["Venus", "Tierra", "Marte", "Jupiter"]
+
+// Agregar elementos
+planetas.push("Saturno")  // Agrega al final
+console.log("Después de push:", planetas)
+
+// Eliminar elementos
+planetas.pop()  // Elimina el último
+console.log("Después de pop:", planetas)
+
+planetas.unshift("Mercurio") // Agrega al inicio
+console.log("Después de unshift:", planetas)
+
+planetas.shift() // Elimina el primero
+console.log("Después de shift:", planetas)
+
+//Propiedades útiles de los arreglos
+console.log("Número de planetas:", planetas.length)
+console.log("¿El arreglo incluye a 'Venus'?", planetas.includes("venus"))*/
+
+
+//Ordenar un arreglo
+/*let numeros = [3, 4, 1, 5, 7, 2, 6]
+numeros.sort(); // Ordena de menor a mayor (como texto)
+console.log("Ordenados:", numeros)
+
+numeros.reverse(); // Invierte el orden
+console.log("Invertidos:", numeros)*/
+
+//Arreglos multidimensionales
+let matriz = [
+    ["Juan pablo", 18],
+    ["Alan", 19],
+    ["Valeria", 20]
+]
+
+console.log("Edad de pablo:", matriz[1][1]) 
+
